fd16b2a1c384d1552c57333ed3c9006814fe5201
Zahid Hasan <59976895+Zahid6992@users.noreply.github.com>
2020-01-20 00:21:22 +0600

    WorkUpdate_Day3

---
+++ app.py
@@ -0,0 +1,28 @@
+from flask import Flask,render_template,request,session,logging,url_for,redirect
+from sqlalchemy import create_engine
+from sqlalchemy.orm import scoped_session,sessionmaker
+
+from passlib.hash import sha256_crypt
+      
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+	return render_template("home.html")
+
+@app.route("/register")
+def register();
+     return render_template("register.html")
+
+@app.route("/login")
+def register():
+	return render_template("login.html")
+
+@app.route("/")
+def home():
+	return render_template("layout.html")
+
+if __name__ =="__main__":
+	app.run(debug=True)
+
+	
